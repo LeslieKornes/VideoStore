1ab681175e81364afb9414072e01de210f15d7af
Leslie Kornes <Lesliekornes@gmail.com>
2017-11-12 21:44:51 -0500

    added data annotations to customer name prop

--- VideoStore/Models/Customer.cs
+++ VideoStore/Models/Customer.cs
@@ -1,9 +1,15 @@
-﻿namespace VideoStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoStore.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
