84a2193c6f8811544b3c13e94590f0b64121c3be
Leslie Kornes <Lesliekornes@gmail.com>
2017-11-12 19:29:26 -0500

    added index actions and views

--- VideoStore/Controllers/MoviesController.cs
+++ VideoStore/Controllers/MoviesController.cs
@@ -1,32 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using VideoStore.Models;
-using VideoStore.ViewModels;
 
 namespace VideoStore.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
+        public ActionResult Index()
         {
-            var movie = new Movie()
-            {
-                Name = "Shrek!"
-            };
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
-            var viewmodel = new RandomMovieViewModel
-            {
-                Movie = movie, 
-                Customers = customers
-            };
+            var movies = GetMovies();
 
-            return View(viewmodel);
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name="Shrek!"},
+                new Movie {Id = 2, Name = "Wall-e" } 
+            };
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
